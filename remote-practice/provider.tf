63401bfccea68b4ec27094269cfe1c5f337b3440
harishreddy1087 <harishreddy1087@gmail.com>
2026-02-02 03:18:28 +0530

    checking the result

--- remote-practice/provider.tf
+++ remote-practice/provider.tf
@@ -5,7 +5,8 @@ terraform {
       version = "6.28.0"
     }
   }
-
+################3 
+# below block is mandatory is store the state file in S3 and lock with DynamoDB
   backend "s3" {
 bucket = "my-terraform-state-bucket"
 key = "global/s3/terraform.tfstate"
