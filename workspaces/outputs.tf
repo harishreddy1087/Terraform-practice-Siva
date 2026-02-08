b6dc978f9e8bfb6af2df7f4f1d9c42b74eb3002b
harishreddy1087 <harishreddy1087@gmail.com>
2026-02-09 00:34:01 +0530

    checking the result

--- workspaces/outputs.tf
+++ workspaces/outputs.tf
@@ -1,6 +1,6 @@
 output "public_ip" {
   value = aws_instance.jems[0].public_ip # here im usisg the [0] because i want the public of the one instances because i have launched the 2instance
-
+# if you gave [*] menas you will get the all the instances ips
 
 }
 
